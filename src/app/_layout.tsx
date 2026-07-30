0472bf977bb1ac2fd8fc15c0412b00d905c3e3dc
Phanupphong-pc <wikomakro47@gmail.com>
2026-07-30 16:12:23 +0700

    d

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,15 @@ export default function Layout() {
             <Tabs.Screen
                 name="index"
                 options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color }) => <Ionicons name="home"
+                        size={24} color={color} />,
+
+                }}
+            />
+            <Tabs.Screen
+                name="dashboard"
+                options={{
                     title: 'Dashboard',
                     tabBarIcon: ({ color }) => <Ionicons name="document-text"
                         size={24} color={color} />,
